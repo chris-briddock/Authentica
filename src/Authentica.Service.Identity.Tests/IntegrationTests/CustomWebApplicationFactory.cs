0ef9b6e3f4cdc71e34c9fba874d709164c8afd0c
Christopher Briddock <briddockchristopher@gmail.com>
2024-08-04 16:14:57 +0100

    Increase wait to accomodate pipelines errors.

--- src/Authentica.Service.Identity.Tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ src/Authentica.Service.Identity.Tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -11,7 +11,7 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
     public void StartTestContainer()
     {
         _msSqlContainer.StartAsync().Wait();
-        Task.Delay(TimeSpan.FromSeconds(15)).Wait();
+        Task.Delay(TimeSpan.FromSeconds(30)).Wait();
     }
     public void StopTestContainer()
     {
