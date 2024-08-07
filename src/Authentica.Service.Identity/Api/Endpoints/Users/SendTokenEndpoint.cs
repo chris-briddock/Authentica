bf27a5b8dcc360882bba973b51da381b195d9ea9
Christopher Briddock <briddockchristopher@gmail.com>
2024-08-07 19:10:29 +0100

    amend publisher and token test.

--- src/Authentica.Service.Identity/Api/Endpoints/Users/SendTokenEndpoint.cs
+++ src/Authentica.Service.Identity/Api/Endpoints/Users/SendTokenEndpoint.cs
@@ -5,7 +5,6 @@ using Ardalis.ApiEndpoints;
 using Authentica.Common;
 using Domain.Aggregates.Identity;
 using Domain.Events;
-using MassTransit;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -55,7 +54,7 @@ public class SendTokenEndpoint : EndpointBaseAsync
     {
         EmailMessage message = new();
         var userManager = Services.GetRequiredService<UserManager<User>>();
-        var emailPublisher = Services.GetRequiredService<IBus>();
+        var emailPublisher = Services.GetRequiredService<IEmailPublisher>();
         var eventStore = Services.GetRequiredService<IEventStore>();
 
         User? user = await userManager.FindByEmailAsync(request.Email)!;
