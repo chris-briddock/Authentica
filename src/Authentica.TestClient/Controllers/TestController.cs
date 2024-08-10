4b236d3232d214123f1d9fad8d6c010291c5472b
Christopher Briddock <briddockchristopher@gmail.com>
2024-08-10 19:28:13 +0100

    Update FeatureFlags, rename TestClient.

--- src/Authentica.TestClient/Controllers/TestController.cs
+++ src/Authentica.TestClient/Controllers/TestController.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TestClient.Controllers;
+namespace Authentica.TestClient.Controllers;
 
 
 [ApiController]
--- src/Authentica.TestClient/ServiceCollectionExtensions.cs
+++ src/Authentica.TestClient/ServiceCollectionExtensions.cs
@@ -2,7 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
-namespace TestClient;
+namespace Authentica.TestClient;
 
 public static class ServiceCollectionExtensions
 {
